604a0af3724add81424d7cf64536c86d357ce9df
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2025-08-23 12:53:58 +0400

    feat: Add application main class

---
+++ src/test/java/ryzendee/app/AuditSearchServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package ryzendee.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class AuditSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
